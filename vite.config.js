943f059fddba13b0c0a8e99675a66301d3488741
Benedikt Bergenthal <benedikt@escsoftware.de>
2023-08-24 18:03:48 +0200

    removed unneeded imports from vite.config.js
    Signed-off-by: Benedikt Bergenthal <benedikt@escsoftware.de>

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import vueI18n from '@intlify/vite-plugin-vue-i18n'
-import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
-import { fileURLToPath } from 'url'
-import vue from '@vitejs/plugin-vue'
-import Pages from 'vite-plugin-pages'
-import { resolve, dirname } from 'node:path'
-import path from 'path'
-import eslintPlugin from 'vite-plugin-eslint'
+import { defineConfig } from 'vite';
+// import vueI18n from '@intlify/vite-plugin-vue-i18n';
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
+import { fileURLToPath } from 'url';
+import vue from '@vitejs/plugin-vue';
+import Pages from 'vite-plugin-pages';
+import { resolve, dirname } from 'node:path';
+import path from 'path';
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -23,14 +23,14 @@ export default defineConfig({
     Pages(),
     eslintPlugin(),
     VueI18nPlugin({
-        include: [path.resolve(__dirname, '../src/assets/locale/**')],
+      include: [path.resolve(__dirname, '../src/assets/locale/**')],
 
     }),
   ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      'PUBLIC_DIR': path.resolve(__dirname, './public'),
+      PUBLIC_DIR: path.resolve(__dirname, './public'),
     },
   },
 });
